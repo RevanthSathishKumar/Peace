e64f738ac8d7639cefb1a45f5eca52937781532d
RevanthSathishKumar K <ksathishk9@gmail.com>
2014-01-13 13:31:35 +0000

    Create my second rails application, Peace.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peace::Application.config.secret_key_base = 'edb4303b5f7e4b3a534766071987e294f724af513abbd7c744885b18aff163d7cca804fa2818bec9de495a289721de8123ef0915110820ccf87b5eaea9f8c5b0'
